ad7d623c001c31d2e367260b1cc61538c53572f1
rehan7594 <rehansdm94@gmail.com>
2021-04-25 04:14:58 -0700

    Added: last_name_validation

--- main/java/com/userregistrationform/UserRegistrationForm.java
+++ main/java/com/userregistrationform/UserRegistrationForm.java
@@ -3,7 +3,15 @@ package com.userregistrationform;
 import java.util.regex.Pattern;
 
 public class UserRegistrationForm {
+	
+	// UC1 - to check first name
 	public boolean firstName(String name) {
 		return (Pattern.matches("^[A-Z]{1}[a-z]{2,}$", name) == true);
 	}
+	
+	 // UC2-to validate last name
+    public boolean lastName(String name)
+    {
+        return (Pattern.matches("^[A-Z]{1}[a-z]{3,}", name));
+    }
 }
--- test/java/com/userregistrationform/UserRegistrationFormTest.java
+++ test/java/com/userregistrationform/UserRegistrationFormTest.java
@@ -11,14 +11,29 @@ public class UserRegistrationFormTest {
 	public void givenFirstName_ShouldReturnFalse() {
 		Assert.assertFalse(user.firstName("yas"));
 	}
-	
+
 	@Test
 	public void givenFirstNmae_ShouldReturnTrue()
 	{
 		Assert.assertTrue(user.firstName("Yash"));
 	}
-	
 
+	@Test
+	// UC2-to validate last name if first letter is in upperCase
+	public void givenLastName_WhereCapitaLetter_ShouldReturnTrue()
+	{
+		UserRegistrationForm user= new UserRegistrationForm();
+		boolean check = user.lastName("Kumar");
+		Assert.assertEquals(true,check);
+	}
+	@Test
+	//UC2-to validate last name if first letter is in lowerCase
+	public void givenLastName_WhereLowerCase_ShouldReturnFalse()
+	{
+		UserRegistrationForm user= new UserRegistrationForm();
+		boolean check = user.lastName("kumar");
+		Assert.assertEquals(false,check);
+	}
 }
 
 
