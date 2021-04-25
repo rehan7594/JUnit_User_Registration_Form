1dc26fde4df7de4b3f87af68562727877f4b078c
rehan7594 <rehansdm94@gmail.com>
2021-04-25 06:42:11 -0700

    Added: validating firstName,lastName,email,password and mobile number

--- main/java/com/userregistrationform/UserRegistrationForm.java
+++ main/java/com/userregistrationform/UserRegistrationForm.java
@@ -4,15 +4,25 @@ import java.util.regex.Pattern;
 
 public class UserRegistrationForm {
 
+
 	// UC1 - to check first name
-	public boolean firstName(String name) {
-		return (Pattern.matches("^[A-Z]{1}[a-z]{2,}$", name) == true);
+	public String firstName(String name)
+	{
+		boolean result = (Pattern.matches("^[A-Z]{1}[a-z]{3,}$", name) == true);
+		if(result == true)
+			return "Happy";
+		else
+			return "Sad";
 	}
 
 	// UC2-to validate last name
-	public boolean lastName(String name)
+	public String lastName(String name)
 	{
-		return (Pattern.matches("^[A-Z]{1}[a-z]{3,}", name));
+		boolean check = (Pattern.matches("^[A-Z]{1}[a-z]{3,}", name));
+		if(check == true)
+			return "Happy";
+		else
+			return "Sad";
 	}
 
 	/*UC 3 - enter a valid email
@@ -20,54 +30,72 @@ public class UserRegistrationForm {
 	 * @return string:- return boolean value true or false
 	 * Email has 3 parts abc, bl & co
 	 */
-	public boolean emailId(String email)
+	public String emailId(String email)
 	{
-		return (Pattern.matches("[a-zA-Z0-9][a-zA-Z0-9_.]*@[a-zA-Z0-9]+([.][a-zA-Z]+)+", email) == true);
+		boolean check = (Pattern.matches("[a-zA-Z0-9][a-zA-Z0-9_.]*@[a-zA-Z0-9]+([.][a-zA-Z]+)+", email) == true);
+		if(check == true)
+			return "Happy";
+		else
+			return "Sad";
 	}
-	
-	// UC4-validating mobile number
-    public boolean mobileNo(String number)
-    {
-        return(Pattern.matches("(0|91)?\\s{1}[6-9][0-9]{9}", number) == true);
-    }
-    
-  //UC5-validating password with minimum 8 characters
-    public boolean passwordCheck(String password)
-    {
-        return (Pattern.matches("[A-Z0-9a-z$%&*@!#]{8,}", password) == true);
-    }
-    
-  //UC6-validating password having atleast 1 upperCase character should return true
-    public boolean passwordCheckForAtLeastOneUpperCase(String password)
-    {
-        return ((Pattern.matches("[A-Z0-9a-z$%&*@!#]{8,}", password)
-                && Pattern.matches(".*[A-Z].*", password)) == true);
-    }
-    
-    /*UC-7-validating password having atleast 8 character
-     * password having minimum 1 uppeCase character and
-     * password will have minimum 1 numeric value
-     */
-      public boolean passwordCheck_ForAtLeast_OneNumericCharacter(String number)
-      {
-          return ((Pattern.matches("[A-Z0-9a-z$%&*@!#]{8,}", number)
-                  && Pattern.matches(".*[A-Z].*", number)
-                  && Pattern.matches(".*[0-9].*", number)) == true);
-      }
-      
-    //UC8-validating password for exactly 1 special character
-      public boolean passwordCheck_ForExactly_OneSpecialCharacter(String password)
-      {
-          return ((Pattern.matches("[A-Z0-9a-z$%&*@!#]{8,}", password)
-                  && Pattern.matches(".*[A-Z].*", password)
-                  && Pattern.matches(".*[0-9].*", password)
-                  && Pattern.matches(".*[\\W]{1}.*", password)) == true);
 
-      }
-      
-    //UC9-validating email
-      public boolean emailCheck(String email)
-      {
-          return (Pattern.matches("[a-z A-Z 0-9][a-z A-Z 0-9_.]*@[a-zA-Z0-9]+([.][a-zA-Z]+)+", email)) == true;
-      }
+	// UC4-validating mobile number
+	public String mobileNo(String number)
+	{
+		boolean check = (Pattern.matches("(0|91)?\\s{1}[6-9][0-9]{9}", number) == true);
+		if(check == true)
+			return "Happy";
+		else
+			return "Sad";
+	}
+
+	//UC5-validating password with minimum 8 characters
+	public String passwordCheck(String password)
+	{
+		boolean check = (Pattern.matches("[A-Z0-9a-z$%&*@!#]{8,}", password) == true);
+		if(check == true)
+			return "Happy";
+		else
+			return "Sad";
+	}
+
+	//UC6-validating password having atleast 1 upperCase character should return true
+	public String passwordCheck_ForAtLeast_OneUpperCase(String password)
+	{
+		boolean result = ((Pattern.matches("[A-Z0-9a-z$%&*@!#]{8,}", password)
+				&& Pattern.matches(".*[A-Z].*", password)) == true);
+		if(result == true)
+			return "Happy";
+		else
+			return "Sad";
+	}
+
+	/*UC-7-validating password having atleast 8 character
+	 * password having minimum 1 uppeCase character and
+	 * password will have minimum 1 numeric value
+	 */
+	public String passwordCheck_ForAtLeast_OneNumericCharacter(String number)
+	{
+		boolean check = ((Pattern.matches("[A-Z0-9a-z$%&*@!#]{8,}", number)
+				&& Pattern.matches(".*[A-Z].*", number)
+				&& Pattern.matches(".*[0-9].*", number)) == true);
+		if(check == true)
+			return "Happy";
+		else
+			return "Sad";
+	}
+
+	//UC8-validating password for exactly 1 special character should return Happy
+	public String passwordCheck_ForExactly_OneSpecialCharacter(String password) {
+		boolean check = ((Pattern.matches("[A-Z0-9a-z$%&*@!#]{8,}", password)
+				&& Pattern.matches(".*[A-Z].*", password)
+				&& Pattern.matches(".*[0-9].*", password)
+				&& Pattern.matches(".*[\\W]{1}.*", password)) == true);
+		if(check == true)
+			return "Happy";
+		else
+			return "Sad";
+	}
+
 }
+
--- test/java/com/userregistrationform/UserRegistrationFormTest.java
+++ test/java/com/userregistrationform/UserRegistrationFormTest.java
@@ -7,132 +7,129 @@ public class UserRegistrationFormTest {
 	UserRegistrationForm user= new UserRegistrationForm();
 
 	@Test
-	//UC1-to validate first name should return false
-	public void givenFirstName_ShouldReturnFalse() {
-		Assert.assertFalse(user.firstName("yas"));
+	//UC1-to validate first name should return Happy
+	public void firstNameValidation_ShouldReturnHappy() {
+
+		String result = user.firstName("Rehan");
+		Assert.assertEquals("Happy",result);
+	}
+	@Test
+	//UC1-to validate first name should return Sad
+	public void givenFirstName_WhereLowerCase_ShouldReturnSad() {
+
+		String  result = user.firstName("ras");
+		Assert.assertEquals("Sad", result);
 	}
 
 	@Test
-	public void givenFirstNmae_ShouldReturnTrue()
+	// UC2-to validate last name if first letter is in upperCase should retrun happy
+	public void givenLastName_WhereCapitaLetter_ShouldReturnHappy()
 	{
-		Assert.assertTrue(user.firstName("Yash"));
+		String result = user.lastName("Kumar");
+		Assert.assertEquals("Happy",result);
 	}
 
 	@Test
-	// UC2-to validate last name if first letter is in upperCase
-	public void givenLastName_WhereCapitaLetter_ShouldReturnTrue()
+	//UC2-to validate last name if first letter is in lowerCase should return sad
+	public void givenLastName_WhereLowerCase_ShouldReturnSad()
 	{
-
-		boolean check = user.lastName("Kumar");
-		Assert.assertEquals(true,check);
-	}
-	@Test
-	//UC2-to validate last name if first letter is in lowerCase
-	public void givenLastName_WhereLowerCase_ShouldReturnFalse()
-	{
-
-		boolean check = user.lastName("kumar");
-		Assert.assertEquals(false,check);
+		String result = user.lastName("kumar");
+		Assert.assertEquals("Sad",result);
 	}
 
 	@Test
-	//UC3-to validate emailID
-	public void givenEmailId_ShouldReturnTrue()
+	//UC3-to validate emailID should return Happy
+	public void givenEmailId_ShouldReturnHappy()
 	{
-
-		boolean check = user.emailId("rehansdm94@gmail.com");
-		Assert.assertEquals(true,check);
+		String  result = user.emailId("rehansdm94@gmail.com");
+		Assert.assertEquals("Happy",result);
 	}
 
 	@Test
-	// UC4-validating mobile number with country code followed by space and number
-	public void givenMobileNumber_ShouldReturnTrue()
+	//UC3-to validate emailID should return Sad
+	public void givenEmailId_ShouldReturnSad()
 	{
-
-		boolean check = user.mobileNo("91 9134567872");
-		Assert.assertEquals(true,check);
+		String  result = user.emailId("@123@gmail.com");
+		Assert.assertEquals("Sad",result);
 	}
-	//UC4-validating mobile number without country code followed by space
+
 	@Test
-	public void givenMobileNumber_ShouldReturnFalse()
+	// UC4-validating mobile number with country code followed by space and number should return Happy
+	public void givenMobileNumber_ShouldReturnHappy()
 	{
-
-		boolean check = user.mobileNo("9123456624");
-		Assert.assertEquals(false,check);
+		String check = user.mobileNo("91 9134567872");
+		Assert.assertEquals("Happy",check);
 	}
-	
-	//UC5-validating for password with minimum 8 characters should return true
-    public void givenPassword_ShouldReturnTrue()
-    {
-        
-        boolean check = user.passwordCheck("qwerty123@");
-        Assert.assertEquals(true,check);
-    }
-    @Test
-    //UC5-validating password having less than 8 characters should return false
-    public void givenPassword_ShouldReturnFalse()
-    {
-        
-        boolean check = user.passwordCheck("yetet@");
-        Assert.assertEquals(false,check);
-    }
-    
-    @Test
-    //UC6-validating password having minimum 1 upperCase character should return true
-    public void givenPassword_ForMinimumOneUpperCase_ShouldReturnTrue()
-    {
-        
-        boolean check = user.passwordCheckForAtLeastOneUpperCase("Qwerty123@");
-        Assert.assertEquals(true,check);
-    }
-    @Test
-    //UC6-validating password having no upperCase character should return false
-    public void givenPassword_WithNoUpperCase_ShouldReturnFalse()
-    {
-       
-        boolean check = user.passwordCheckForAtLeastOneUpperCase("qwerty123@");
-        Assert.assertEquals(false,check);
-    }
-    
-    @Test
-    /*UC-7-validating password having minimum 8 character
-     * password having minimum 1 upperCase character and
-     * password will have minimum 1 numeric value
-     */
-    public void givenPassword_WithMinimumOneNumber_ShouldReturnTrue() {
-        
-        boolean check = user.passwordCheck_ForAtLeast_OneNumericCharacter("Qwerty123@");
-        Assert.assertEquals(true, check);
-    }
-    
-    @Test
-    /*UC8-validating password for exactly one special character should return true
-    * and all rule should passed
-    */
-    public void givenPassword_WithExactlyOneSpecialCharacter_ShouldReturnTrue()
-    {
-        
-        boolean check = user.passwordCheck_ForExactly_OneSpecialCharacter("Qwerty12@");
-        Assert.assertEquals(true,check);
-    }
-    
-    @Test
-    //UC9-validating email should return true
-    public void givenEmail_ShouldReturnTrue()
-    {
-        
-        boolean check = user.emailCheck("yash123@gmil.com");
-        Assert.assertEquals(true,check);
-    }
 
-    @Test
-    //UC9-validating email should return false
-    public void givenEmail_ShouldReturnFalse()
-    {
-        
-        boolean check = user.emailCheck("@gmil.com");
-        Assert.assertEquals(false,check);
-    }
+	//UC4-validating mobile number without country code followed by space should return Sad
+	@Test
+	public void givenMobileNumber_ShouldReturnSad()
+	{
+		String check = user.mobileNo("9123456624");
+		Assert.assertEquals("Sad",check);
+	}
+
+	@Test
+	//UC5-validating for password with minimum 8 characters should return Happy
+	public void givenPassword_ShouldReturnHappy()
+	{
+		String check = user.passwordCheck("qwerty123@");
+		Assert.assertEquals("Happy",check);
+	}
+
+	@Test
+	//UC5-validating password having less than 8 characters should return Sad
+	public void givenPassword_ShouldReturnSad()
+	{
+		String check = user.passwordCheck("yetet@");
+		Assert.assertEquals("Sad",check);
+	}
+
+	@Test
+	//UC6-validating password having minimum 1 upperCase character should return Happy
+	public void givenPassword_ForMinimumOneUpperCase_ShouldReturnHappy()
+	{
+		String check = user.passwordCheck_ForAtLeast_OneUpperCase("Qwerty123@");
+		Assert.assertEquals("Happy",check);
+	}
+
+	@Test
+	//UC6-validating password having no upperCase character should return Sad
+	public void givenPassword_WithNoUpperCase_ShouldReturnSad()
+	{
+		String check = user.passwordCheck_ForAtLeast_OneUpperCase("qwerty123@");
+		Assert.assertEquals("Sad",check);
+	}
+
+	@Test
+	/*UC7-validating password having minimum 8 character
+	 * password having minimum 1 upperCase character and
+	 * password will have minimum 1 numeric value
+	 */
+	public void givenPassword_WithMinimumOneNumber_ShouldReturnHappy() {
+		String check = user.passwordCheck_ForAtLeast_OneNumericCharacter("Qwerty123@");
+		Assert.assertEquals("Happy", check);
+	}
+
+	@Test
+	/*UC8-validating password for exactly one special character should return true
+	 * and all rule should passed
+	 */
+	public void givenPassword_WithExactlyOneSpecialCharacter_ShouldReturnHappy()
+	{
+		String  check = user.passwordCheck_ForExactly_OneSpecialCharacter("Qwerty12@");
+		Assert.assertEquals("Happy",check);
+	}
+
+	@Test
+	/*UC8-validating password for exactly no special character should return sad
+	 * and all rule should passed
+	 */
+	public void givenPassword_WithExactlyOneSpecialCharacter_ShouldReturnSad()
+	{
+		String  check = user.passwordCheck_ForExactly_OneSpecialCharacter("Qwerty12");
+		Assert.assertEquals("Sad",check);
+	}
 
 }
 
