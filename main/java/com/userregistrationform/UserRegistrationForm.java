b1a13e0b70e76efb8efeaefcf1c2aef96058b7a3
rehan7594 <rehansdm94@gmail.com>
2021-04-25 04:47:06 -0700

    Added: mobileNo validation

--- main/java/com/userregistrationform/UserRegistrationForm.java
+++ main/java/com/userregistrationform/UserRegistrationForm.java
@@ -3,15 +3,31 @@ package com.userregistrationform;
 import java.util.regex.Pattern;
 
 public class UserRegistrationForm {
-	
+
 	// UC1 - to check first name
 	public boolean firstName(String name) {
 		return (Pattern.matches("^[A-Z]{1}[a-z]{2,}$", name) == true);
 	}
+
+	// UC2-to validate last name
+	public boolean lastName(String name)
+	{
+		return (Pattern.matches("^[A-Z]{1}[a-z]{3,}", name));
+	}
+
+	/*UC 3 - enter a valid email
+	 *@description-validating emailId is valid or not
+	 * @return string:- return boolean value true or false
+	 * Email has 3 parts abc, bl & co
+	 */
+	public boolean emailId(String email)
+	{
+		return (Pattern.matches("[a-zA-Z0-9][a-zA-Z0-9_.]*@[a-zA-Z0-9]+([.][a-zA-Z]+)+", email) == true);
+	}
 	
-	 // UC2-to validate last name
-    public boolean lastName(String name)
+	// UC4-validating mobile number
+    public boolean mobileNo(String number)
     {
-        return (Pattern.matches("^[A-Z]{1}[a-z]{3,}", name));
+        return(Pattern.matches("(0|91)?\\s{1}[6-9][0-9]{9}", number) == true);
     }
 }
--- test/java/com/userregistrationform/UserRegistrationFormTest.java
+++ test/java/com/userregistrationform/UserRegistrationFormTest.java
@@ -7,7 +7,7 @@ public class UserRegistrationFormTest {
 	UserRegistrationForm user= new UserRegistrationForm();
 
 	@Test
-	// to validate first name should return false
+	//UC1-to validate first name should return false
 	public void givenFirstName_ShouldReturnFalse() {
 		Assert.assertFalse(user.firstName("yas"));
 	}
@@ -22,7 +22,7 @@ public class UserRegistrationFormTest {
 	// UC2-to validate last name if first letter is in upperCase
 	public void givenLastName_WhereCapitaLetter_ShouldReturnTrue()
 	{
-		UserRegistrationForm user= new UserRegistrationForm();
+		
 		boolean check = user.lastName("Kumar");
 		Assert.assertEquals(true,check);
 	}
@@ -30,10 +30,36 @@ public class UserRegistrationFormTest {
 	//UC2-to validate last name if first letter is in lowerCase
 	public void givenLastName_WhereLowerCase_ShouldReturnFalse()
 	{
-		UserRegistrationForm user= new UserRegistrationForm();
+		
 		boolean check = user.lastName("kumar");
 		Assert.assertEquals(false,check);
 	}
+	
+	@Test
+    //UC3-to validate emailID
+    public void givenEmailId_ShouldReturnTrue()
+    {
+        
+        boolean check = user.emailId("rehansdm94@gmail.com");
+        Assert.assertEquals(true,check);
+    }
+	
+	 @Test
+	    // UC4-validating mobile number with country code followed by space and number
+	    public void givenMobileNumber_ShouldReturnTrue()
+	    {
+	        
+	        boolean check = user.mobileNo("91 9134567872");
+	        Assert.assertEquals(true,check);
+	    }
+	    //UC4-validating mobile number without country code followed by space
+	    @Test
+	    public void givenMobileNumber_ShouldReturnFalse()
+	    {
+	       
+	        boolean check = user.mobileNo("9123456624");
+	        Assert.assertEquals(false,check);
+	    }
 }
 
 
